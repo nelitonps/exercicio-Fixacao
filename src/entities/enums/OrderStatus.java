94e04884b103981eeca565a52c674f068db8e6d1
Neliton PS <neliton.net@hotmail.com>
2020-08-12 16:46:37 -0300

    Projeto concluido

---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package entities.enums;
+
+public enum OrderStatus {
+
+	//importante lembrar que usa-se sempre letras em maiúsculos e anderline para separação
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED
+	
+}
